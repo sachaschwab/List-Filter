47f33546f37a66ce26d7ed6740285b9d21783ff0
Sacha <sacha.schwab@gmail.com>
2020-11-24 12:53:32 +0100

    Initial Commit

---
+++ List-Filter/List_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_FilterApp.swift
+//  List-Filter
+//
+//  Created by Sacha Schwab on 24.11.20.
+//
+
+import SwiftUI
+
+@main
+struct List_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
